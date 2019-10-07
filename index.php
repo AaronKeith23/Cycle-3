8bff03f2751dff3a05bd7887cea095bba00b6982
AaronKeith23 <54514908+AaronKeith23@users.noreply.github.com>
2019-10-07 17:55:13 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: aaronkeith
+ * Date: 10/7/2019
+ * Time: 4:54 PM
+ */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$pagetitle = "Homepage";
+include_once "header.php";
+?>
+
+<h2>This website is for anyone to store their files on a school database like how we use on Moodle!</h2>
+<img src="images/ccu.jpg" alt="College" class="center">
+<p>Welcome to my Cycle 3 Project, for a School File Storage System.</p>
+<?php
+"<br></br>";
+require_once "footer.php";
+?>
+
+
